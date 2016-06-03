df73a61811cc4c7ee5df547a6c30e43d9be7d21c
alternatekev <alternatekev@me.com>
2016-06-03 16:20:37 -0600

    initial commit

---
+++ alternate-lib/widgets/alt-widget.php
@@ -0,0 +1,14 @@
+<?php
+
+class AltWidget extends WP_Widget {
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+
+		foreach ( $new_instance as $key => $value ) {
+			$instance[ $key ] = ( ! empty( $new_instance[ $key ] ) ) ? strip_tags( $new_instance[ $key ] ) : '';
+		}
+		return $instance;
+	}
+
+}
